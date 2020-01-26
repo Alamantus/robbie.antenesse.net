8e26c60f3923176111fd31a00cba891bcc364035
Robbie Antenesse <dev@alamantus.com>
2020-01-26 00:11:42 -0700

    Update line break syntax for markdown
    Interpret `\\` as `\n` so I can still do markdown in the minified html

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,5 @@ import md from 'snarkdown';
 const markdownSections = document.getElementsByClassName('markdown');
 for (let i = 0; i < markdownSections.length; i++) {
   const section = markdownSections[i];
-  section.innerHTML = md(section.innerText.replace(/\\n/g, '\n'));
+  section.innerHTML = md(section.innerText.replace(/\\\\/g, '\n'));
 }
